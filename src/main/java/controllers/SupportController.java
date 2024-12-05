2347df03a551b361bb046b5447f8c7ccc1271b98
24097691 <24097691@student.hhs.nl>
2024-12-05 11:26:40 +0100

    All home buttons done (no icons)

---
+++ src/main/java/controllers/SupportController.java
@@ -0,0 +1,9 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+
+public class SupportController {
+    public void goToHome(ActionEvent event) {
+        SceneController.goToHome();
+    }
+}
