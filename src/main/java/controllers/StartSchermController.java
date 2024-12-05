3711c78f6bf31f5d8ad6dc7cb30b1685524523de
24097691 <24097691@student.hhs.nl>
2024-12-05 10:19:52 +0100

    switched long method to a method call

--- src/main/java/controllers/StartSchermController.java
+++ src/main/java/controllers/StartSchermController.java
@@ -10,14 +10,15 @@ import java.io.IOException;
 public class StartSchermController {
 
     public void goToStart(ActionEvent event) {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/Mockup_SceneBuilder_Start.fxml"));
-            Parent root = loader.load();
-            Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
-            stage.setScene(new Scene(root));
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/Mockup_SceneBuilder_Start.fxml"));
+//            Parent root = loader.load();
+//            Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+//            stage.setScene(new Scene(root));
+//            stage.show();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        SceneController.sceneswitch("/views/Mockup_SceneBuilder_Start.fxml");
     }
 }
