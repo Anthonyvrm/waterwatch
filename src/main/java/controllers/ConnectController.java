01b37fcad9476a85463108927c0128b7c1b3a2d9
anthonyvrm <anthonyvrm@gmail.com>
2025-01-27 16:49:23 +0100

    login en register schermen

--- src/main/java/controllers/ConnectController.java
+++ src/main/java/controllers/ConnectController.java
@@ -6,4 +6,11 @@ public class ConnectController {
     public void goToHome(ActionEvent event) {
         SceneController.goToHome();
     }
+    public void goToLogin() {
+        SceneController.sceneswitch("/views/Login.fxml"); // Switch to the login scene
+    }
+
+    public void goToRegister() {
+        SceneController.sceneswitch("/views/registreren.fxml"); // Switch to the registration scene
+    }
 }
